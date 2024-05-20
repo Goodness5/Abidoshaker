aa09dcd334faa2b486e7446aa09ebdc11aa44e5c
superman <goodnesskolapo@gmail.com>
2024-05-20 19:44:48 +0100

    in progress ...

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,208 @@
 use clap::Parser;
-use std::{path::PathBuf, process::{Command, Output}};
+use serde_derive::Deserialize;
+use std::{fs, path::PathBuf, process::{Command, exit}};
+use toml;
+use starknet::{contract::ContractFactory, core::types::FieldElement, signers::SigningKey};
 
-#[derive(Parser)]
+
+
+#[derive(Parser, Clone)]
 #[command(version = "1.0", about = "Automates the process of deploying contracts on StarkNet.")]
 struct Arguments {
     /// Path to the directory containing the contract to deploy.
     #[arg(short = 'p', long)]
     path: PathBuf,
 
-    /// Pattern to match against the contract files.
-    #[arg(short = 't', long)]
-    pattern: String,
+    /// Wallet address deploying contract
+    #[clap(short = 'd', long = "wallet-address")]
+    public_key: String,
 
-    /// API key for Infura.
-    #[clap(short, long)]
-    infura_api_key: String,
+    // Contract to be deployed
+    #[clap(short = 'c', long = "contract-name")]
+    contract_name: String,
 
-    /// API key for Alchemy.
-    #[clap(short, long)]
-    alchemy_api_key: String,
+    #[clap(long = "constructor")]
+    constructor_inputs: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct Package {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct Data {
+    package: Package,
+}
+
+fn read_scarb_toml(path: &PathBuf) -> String {
+    let scarb_toml_path = path.join("Scarb.toml");
+    fs::read_to_string(scarb_toml_path.clone()).unwrap_or_else(|_| {
+        eprintln!("Could not read file `{:?}`", scarb_toml_path);
+        exit(1);
+    })
+}
+
+fn parse_package_name(contents: &str) -> String {
+    let data: Data = toml::from_str(contents).unwrap_or_else(|_| {
+        eprintln!("Unable to load data from provided content");
+        exit(1);
+    });
+    data.package.name
+}
+
+fn compile_contract(path: &PathBuf) {
+    println!("Compiling the contract...");
+    let compile_command = Command::new("scarb")
+        .arg("build")
+        .current_dir(path)
+        .status()
+        .expect("Failed to compile the contract");
+    if !compile_command.success() {
+        eprintln!("Compilation failed");
+        exit(1);
+    }
+}
+
+fn generate_keystore() {
+    println!("Generating Keystore...");
+    let generate_keystore_command = Command::new("starkli")
+        .args(&["signer", "keystore", "from-key", "account0_keystore.json"])
+        .output()
+        .expect("Failed to generate account");
+    if !generate_keystore_command.status.success() {
+        eprintln!("Account generation failed");
+    }
+}
+
+fn fetch_account(public_key: &str) {
+    println!("Fetching generated account...");
+    let fetch_account_command = Command::new("starkli")
+        .args(&[
+            "account",
+            "fetch",
+            public_key,
+            "--rpc",
+            "https://starknet-sepolia.public.blastapi.io",
+            "--output",
+            "account0_account.json",
+        ])
+        .status()
+        .expect("Failed to fetch account");
+}
+
+fn declare_contract(contract_file_path: &PathBuf, base_dir: &PathBuf) -> String {
+    println!("Declaring the contract...");
+    let mut binding = Command::new("starkli");
+    let declare_command = binding
+        .args(&[
+            "declare",
+            &contract_file_path.to_str().unwrap(),
+            "--rpc",
+            "https://starknet-sepolia.public.blastapi.io",
+            "--account",
+            "account0_account.json",
+            "--keystore",
+            "account0_keystore.json",
+        ])
+        .current_dir(base_dir);
+    println!("Declare command: {:?}", declare_command);
+    declare_command.status().expect("Failed to declare the contract");
+
+    let output = declare_command.output().expect("Failed to execute declare command");
+    let output_str = String::from_utf8(output.stdout)
+    .expect("Invalid UTF-8 sequence in command output")
+    .trim()
+    .to_string();
+            return output_str;
+}
+
+fn parse_constructor_args(constructor_inputs: &Option<String>) -> Vec<String> {
+    match constructor_inputs {
+        Some(input) => {
+            if input.ends_with(".constructor") {
+                fs::read_to_string(input).map_or_else(
+                    |_| {
+                        eprintln!("Could not read constructor file `{:?}`", input);
+                        exit(1);
+                    },
+                    |contents| {
+                        contents
+                            .trim()
+                            .split(',')
+                            .map(|s| s.trim().to_string())
+                            .collect()
+                    },
+                )
+            } else {
+                input
+                    .split(',')
+                    .map(|s| s.trim().to_string())
+                    .collect()
+            }
+        }
+        None => Vec::new(),
+    }
+    .into_iter()
+    .flat_map(|arg| {
+        arg.split(',')
+            .map(str::trim)
+            .filter(|s| !s.is_empty())
+            .map(String::from)
+            .collect::<Vec<String>>()
+    })
+    .map(|arg| {
+        // Remove only the backslashes while keeping the quotes intact
+        arg.replace(r#"\\(?=[^"])"#, "")
+    })
+    .collect()
+}
+
+fn deploy_contract(class_hash: &str, constructor_args: &[String], path: &PathBuf) {
+    let formatted_constructor_args = constructor_args.join(" ");
+    let deploy_args = [
+        "deploy",
+        "--account",
+        "account0_account.json",
+        class_hash,
+        // " 'mytesttoken' 'MTT' 18 1000000000000 0x0455ea216a2ebc127b8e84c18f096542c62bc29182b0055638b06fa11571426a",
+        &formatted_constructor_args,
+        "--rpc",
+        "https://starknet-sepolia.public.blastapi.io/rpc/v0_7",
+        "--keystore",
+        "account0_keystore.json",
+    ];
+
+    println!("Deploy command: {:?}", deploy_args);
+
+    let deploy_command = Command::new("starkli")
+        .args(&deploy_args)
+        .current_dir(path)
+        .status()
+        .expect("Failed to deploy the contract");
+
+    if !deploy_command.success() {
+        eprintln!("Deploy command failed");
+        exit(1);
+    }
 }
 
 fn main() {
     let args = Arguments::parse();
 
-    // Compile the contract
-    println!("Compiling the contract...");
-    let filename = args.path.join("src");
-    Command::new("scarb")
-        .arg("build")
-        .current_dir(filename.clone())
-        .status()
-        .expect("Failed to compile the contract");
+    let contents = read_scarb_toml(&args.path);
+    let package_name = parse_package_name(&contents);
 
-    // Generate the account
-    println!("Generating account...");
-    let generate_account = Command::new("starkli")
-        .args(&["signer", "keystore", "from-key", "account0_keystore.json"])
-        .current_dir(filename.clone())
-        .output()
-        .expect("Failed to generate account");
+    compile_contract(&args.path);
+    generate_keystore();
+    fetch_account(&args.public_key);
 
-    // Extract the public key from the output
-    let public_key = extract_public_key(&generate_account);
-    println!("Public key: {}", public_key);
+    let target_dev_dir = PathBuf::from("target/dev");
+    let contract_file_path = target_dev_dir.join(format!("{}_{}.contract_class.json", package_name, &args.contract_name));
+    let class_hash = declare_contract(&contract_file_path, &args.path);
 
-    // Fetch the generated account
-    // Function to extract the public key from the command output
-    fn extract_public_key(output: &Output) -> String {
-        let output_str = String::from_utf8_lossy(&output.stdout);
-        let lines: Vec<&str> = output_str.split('\n').collect();
-        let public_key_line = lines.iter().find(|line| line.starts_with("Public key:"));
-    
-        if let Some(public_key_line) = public_key_line {
-            let parts: Vec<&str> = public_key_line.split(':').collect();
-            if let Some(public_key) = parts.get(1) {
-                return public_key.trim().to_string();
-            }
-        }
-    
-        panic!("Failed to extract public key from output");
-    }
-    println!("Fetching generated account...");
-    Command::new("starkli")
-        .args(&[
-            "account",
-            "fetch",
-            &public_key,
-            "--rpc",
-            &format!("https://starknet-goerli.infura.io/v3/{}", args.infura_api_key),
-            "--output",
-            "account0_account.json",
-            ])
-        .current_dir(filename.clone())
-        .status()
-        .expect("Failed to fetch account");
+    println!("Class hash: {}", class_hash);
 
-    // Declare the contract
-    println!("Declaring the contract...");
-    Command::new("starkli")
-        .args(&[
-            "declare",
-            &format!("{}/contract_class.json", args.path.display()),
-            "--rpc",
-            &format!("https://starknet-goerli.infura.io/v3/{}", args.infura_api_key),
-        ])
-        .current_dir(filename.clone())
-        .status()
-        .expect("Failed to declare the contract");
-
-    // Deploy the contract
-    println!("Deploying the contract...");
-    Command::new("starkli")
-        .args(&[
-            "deploy",
-            "CLASS_HASH_DECLARED", // Replace CLASS_HASH_DECLARED with the actual class hash
-            "--rpc",
-            &format!("https://starknet-goerli.alchemyapi.io/v2/{}", args.alchemy_api_key),
-        ])
-        .current_dir(filename)
-        .status()
-        .expect("Failed to deploy the contract");
-
-    println!("Deployment completed.");
+    let constructor_args = parse_constructor_args(&args.constructor_inputs);
+    deploy_contract(&class_hash, &constructor_args, &args.path);
 }
-
